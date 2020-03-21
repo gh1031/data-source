3bad1537a89c98711e267a572cc562f49f84899a
gh1031 <1051421758@qq.com>
2020-03-21 23:04:31 +0800

    refactor: migrate to ts

--- router/goods.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')()
-const goods = require('../controller/goods')
-
-router
-  .get('/', ctx => { ctx.body = 'welcome goods page' })
-  .get('/list', ctx => goods.list(ctx))
-
-module.exports = router.routes()
