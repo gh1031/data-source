b03629c16f3b26c2cb98a45528d4aa6fc18ca30f
gh1031 <1051421758@qq.com>
2020-06-22 10:24:01 +0800

    feat: integration pm2

--- app.ts
+++ app.ts
@@ -12,11 +12,11 @@ const app = new Koa();
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
-// 自定义中间价
-app.use(responseTime());
 // 静态文件
 app.use(serve(__dirname + '/static'));
 app.use(bodyparser());
+// 自定义中间价
+app.use(responseTime());
 app.use(router);
 
 export default app;
